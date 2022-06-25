27285b9fea8264702390fdff3a543903d0a382fe
Marvel E <97315860+Reekofgeek@users.noreply.github.com>
2022-06-25 14:10:48 +0100

    Update wsgi.py

--- I4G006918VEO/wsgi.py
+++ I4G006918VEO/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for I4G017616FD3 project.
+WSGI config for I4G006918VEO project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G017616FD3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G006918VEO.settings')
 
 application = get_wsgi_application()
